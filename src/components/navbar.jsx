f09547a81f78725c34372c61cd3c257d9051869d
VINAY4034 <vinaytiwari9915@gmail.com>
2023-02-03 12:01:05 +0530

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import './navbar.css'
+ export default  function Nav1()
+{
+  return <nav className="navdecor">
+    <ul>
+      <li>HOME</li>
+      <li>ORDERS</li>
+      <li>SEARCH</li>
+      <li>CONTACT US</li>
+    </ul>
+  </nav>
+}
